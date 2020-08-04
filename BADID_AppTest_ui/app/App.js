10e7c98c8297519bc6ab16471e34523911d2ba22
hnOh7011 <kikinokiki00@gmail.com>
2020-08-05 01:10:29 +0900

    Delete unnecessary code and modify indentation

--- BADID_AppTest_ui/app/App.js
+++ BADID_AppTest_ui/app/App.js
@@ -6,64 +6,62 @@
  * @flow
  */
 
-import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
-import ReadString from "./ReadString";
-import SetString from "./SetString";
+import React, {Component} from "react";
+import {StyleSheet, Text, View} from "react-native";
 import MainButtonsTable from './MainButtonsTable';
 
 
 type Props = {};
 export default class App extends Component<Props> {
-  state = { loading: true, drizzleState: null };
+    state = {loading: true, drizzleState: null};
 
-  componentDidMount() {
-    const { drizzle } = this.props;
+    componentDidMount() {
+        const {drizzle} = this.props;
 
-    this.unsubscribe = drizzle.store.subscribe(() => {
-      const drizzleState = drizzle.store.getState();
+        this.unsubscribe = drizzle.store.subscribe(() => {
+            const drizzleState = drizzle.store.getState();
 
-      if (drizzleState.drizzleStatus.initialized) {
-        this.setState({ loading: false, drizzleState });
-      }
-    });
-  }
+            if (drizzleState.drizzleStatus.initialized) {
+                this.setState({loading: false, drizzleState});
+            }
+        });
+    }
 
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
 
-  render() {
-    return (
-              <View style={styles.container}>
-                  <View style={styles.title}>
-                      <Text style={styles.appName}>BADID</Text>
-                  </View>
-                  <View style={styles.buttons}>
-                      <MainButtonsTable/>
-                  </View>
-              </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.title}>
+                    <Text style={styles.appName}>BADID</Text>
+                </View>
+                <View style={styles.buttons}>
+                    <MainButtonsTable/>
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
- container: {
-         flex: 1,
-     },
-     title: {
-         flex: 1,
-         justifyContent: "flex-end",
-         alignItems: "center",
-     },
-     appName: {
-             fontSize: 45,
-         },
+    container: {
+        flex: 1,
+    },
+    title: {
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center",
+    },
+    appName: {
+        fontSize: 45,
+    },
     buttons: {
-            flex: 3.3,
-            padding: 16,
-            paddingTop: 30,
-            justifyContent: 'space-between',
-            padding: 5,
-        },
+        flex: 3.3,
+        // padding: 16,
+        paddingTop: 30,
+        justifyContent: 'space-between',
+        padding: 5,
+    },
 });
